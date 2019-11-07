5caec8c10b0dc058e5ad1a24adabb4dfc311895f
Francesca Capel <capel.francesca@gmail.com>
2019-11-07 10:22:24 +0100

    Fix typo

--- CPU/CPUsoftware/src/instrument/RunInstrument.cpp
+++ CPU/CPUsoftware/src/instrument/RunInstrument.cpp
@@ -42,7 +42,7 @@ void RunInstrument::SignalHandler(int signum) {
  */
 int RunInstrument::LvpsSwitch() {
 
-  switch (this->CmdLine->lvps_) {
+  switch (this->CmdLine->lvps_status) {
     case LvpsManager::ON:
 
       switch (this->CmdLine->lvps_subsystem) {
